ba634db97356f2c383a20951b6a6abe93db808d4
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-31 13:41:22 +0100

    adds js-file / trying out array_unshift()

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const deutscheGerichte = ["Speckkuchen", "Thüringer Rostbratwurst", "Quarkkeulchen", "Sauerbraten"];
+
+deutscheGerichte.unshift("Kartoffelsalat", "Döner", "Grünkohl", "Grün Soß");
+
+console.log(deutscheGerichte);
+console.table(deutscheGerichte);
+
